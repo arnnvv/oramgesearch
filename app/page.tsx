663d917256f5b6b141976ece64c3558d3d47b19d
arnnvv <arnav.900@protonmail.com>
2025-07-27 22:39:10 +0530

    feat(auth): setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home(): string {
-  return "HI";
+import { redirect } from "next/navigation";
+import { globalGETRateLimit } from "@/lib/requests";
+import type { JSX } from "react";
+import { getCurrentSession } from "./actions";
+import { LogoutButton } from "@/components/LogoutButton";
+
+export default async function ProfileContent(): Promise<JSX.Element> {
+  const { user, session } = await getCurrentSession();
+
+  if (session === null) return redirect("/login");
+
+  if (!(await globalGETRateLimit())) {
+    return <div>Too many requests</div>;
+  }
+
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img src={user.picture} alt="profile" />
+      <p>{user.email}</p>
+      <LogoutButton />
+    </>
+  );
 }
